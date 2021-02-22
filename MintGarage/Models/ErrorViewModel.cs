e74228855fe9655c68a18b40bd393e9b50e88a71
seddiqi <seddiqi@sheridancollege.ca>
2021-02-22 13:02:26 -0500

    Add project files.

---
+++ MintGarage/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MintGarage.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
