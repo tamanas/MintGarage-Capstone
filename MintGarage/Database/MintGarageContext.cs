6bbd5d8e6ec9b7b4573f6a25850d7bcc465aa5e1
seddiqi <seddiqi@sheridancollege.ca>
2021-03-14 16:23:13 -0400

    Added model for Product and Category

---
+++ MintGarage/Database/MintGarageContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MintGarage.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MintGarage.Database
+{
+    public class MintGarageContext : DbContext
+    {
+        public MintGarageContext(DbContextOptions<MintGarageContext> option) : base(option)
+{
+
+
+
+}
+
+
+
+public DbSet<Product> Product { get; set; }
+public DbSet<Description> Description { get; set; }
+
+
+
+}
+    }
+
---
+++ MintGarage/Migrations/20210314185913_Initial.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MintGarage.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Description",
+                columns: table => new
+                {
+                    DescriptionID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Color = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ProductDescription = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Material = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Length = table.Column<double>(type: "float", nullable: false),
+                    Width = table.Column<double>(type: "float", nullable: false),
+                    Height = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Description", x => x.DescriptionID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    ProductID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProductName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    ProductPrice = table.Column<double>(type: "float", nullable: false),
+                    DescriptionID = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.ProductID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DescriptionProduct",
+                columns: table => new
+                {
+                    DescriptionID = table.Column<int>(type: "int", nullable: false),
+                    ProductID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DescriptionProduct", x => new { x.DescriptionID, x.ProductID });
+                    table.ForeignKey(
+                        name: "FK_DescriptionProduct_Description_DescriptionID",
+                        column: x => x.DescriptionID,
+                        principalTable: "Description",
+                        principalColumn: "DescriptionID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DescriptionProduct_Product_ProductID",
+                        column: x => x.ProductID,
+                        principalTable: "Product",
+                        principalColumn: "ProductID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DescriptionProduct_ProductID",
+                table: "DescriptionProduct",
+                column: "ProductID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DescriptionProduct");
+
+            migrationBuilder.DropTable(
+                name: "Description");
+
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
