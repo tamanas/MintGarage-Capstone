c75f5557d4ddf937651afc6840db151078c27cf1
dawoode <dawoode@sheridancollege.ca>
2021-10-14 17:16:40 -0400

    View Update

--- MintGarage/Controllers/AboutUsController.cs
+++ MintGarage/Controllers/AboutUsController.cs
@@ -1,30 +1,56 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using MintGarage.Models;
 using MintGarage.Models.AboutUsTab.Teams;
 using MintGarage.Models.AboutUsTab.Values;
+using MintGarage.Models.FooterContents.FooterContactInfo;
+using MintGarage.Models.FooterContents.FooterSocialMedias;
+using MintGarage.Models.HomeTab.HomeContents;
 using MintGarage.Models.Partners;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+
 namespace MintGarage.Controllers
 {
     public class AboutUsController : Controller
     {
-
+        public IPartnerRepository partnerRepository;
+        private IFooterContactInfoRepository footerContactInfoRepo;
+        private IFooterSocialMediaRepository footerSocialMediaRepo;
         private ITeamRepository teamRepo;
         private IValueRepository valueRepo;
 
-        public AboutUsController(ITeamRepository teamRepository, IValueRepository valueRepository)
+        private IWebHostEnvironment hostEnv;
+        private string imageFolder = "/Images/";
+
+        public AboutUsController(ITeamRepository teamRepository, IValueRepository valueRepository, IFooterSocialMediaRepository socialMediaRepository,
+                                            IFooterContactInfoRepository contactRepository, IPartnerRepository partnerRepo, IWebHostEnvironment hostEnvironment)
         {
+            partnerRepository = partnerRepo;
             teamRepo = teamRepository;
             valueRepo = valueRepository;
+            footerSocialMediaRepo = socialMediaRepository;
+            footerContactInfoRepo = contactRepository;
+            hostEnv = hostEnvironment;
         }
 
         public IActionResult Index()
         {
-            return View();
+            ViewBag.Partners = partnerRepository.Partners;
+            ViewBag.SocialMedias = footerSocialMediaRepo.FooterSocialMedias;
+            ViewBag.Contacts = footerContactInfoRepo.FooterContactInfo;
+            AboutUsModel aboutUs = new AboutUsModel()
+            
+            {
+                Teams = teamRepo.Teams,
+                Values = valueRepo.Values,
+            };
+            return View(aboutUs);
         }
+
 
         public IActionResult Update()
         {
