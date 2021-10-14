a4046c91d3b91ffb2876564611fc74a93c4a74c0
seddiqi <seddiqi@sheridancollege.ca>
2021-10-14 02:28:33 -0400

    Gallery Table View, Add, Update and Delete functions

---
+++ MintGarage/Models/GalleryModel.cs
@@ -0,0 +1,14 @@
+﻿using MintGarage.Models.GalleryTab;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MintGarage.Models
+{
+    public class GalleryModel
+    {
+        public IEnumerable<Gallery> Galleries { get; set; }
+        public Gallery Gallery { get; set; }
+    }
+}
