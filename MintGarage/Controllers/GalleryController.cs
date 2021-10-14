db681a0fca7881fdbaad3ce6f07d890ea5840298
marasiti <marasiti@sheridancollege.ca>
2021-10-14 14:24:17 -0400

    autogenerated controller and added before/after image with sliders

--- MintGarage/Controllers/GalleryController.cs
+++ MintGarage/Controllers/GalleryController.cs
@@ -1,29 +1,218 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using MintGarage.Models.GalleryTab;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MintGarage.Database;
+using MintGarage.Models;
+using MintGarage.Models.GalleryTab;
+using MintGarage.Models.Partners;
+using MintGarage.Models.FooterContents.FooterSocialMedias;
+using MintGarage.Models.FooterContents.FooterContactInfo;
 
 namespace MintGarage.Controllers
 {
     public class GalleryController : Controller
     {
         private IGalleryRepository galleryRepo;
+        public IPartnerRepository partnerRepo;
+        private IFooterContactInfoRepository footerContactInfoRepo;
+        private IFooterSocialMediaRepository footerSocialMediaRepo;
 
-        public GalleryController(IGalleryRepository galleryRepository)
+        private const String AboutUs = "We are specialists in transforming and organizing any room. " +
+            "We take pride in delivering outstanding quality and unique designs for our clients Across Canada & North America.";
+        private IWebHostEnvironment hostEnv;
+        private string imageFolder = "/Images/";
+
+        private readonly MintGarageContext _context;
+
+        public GalleryController(IGalleryRepository galleryRepository, IPartnerRepository partnerRepository, IFooterContactInfoRepository footerContactInfoRepository, IFooterSocialMediaRepository footerSocialMediaRepository, IWebHostEnvironment hostEnvironment, MintGarageContext context)
         {
             galleryRepo = galleryRepository;
+            partnerRepo = partnerRepository;
+            footerContactInfoRepo = footerContactInfoRepository;
+            footerSocialMediaRepo = footerSocialMediaRepository;
+            hostEnv = hostEnvironment;
+            _context = context;
         }
 
-        public IActionResult Index()
+        // GET: Galleries
+        public async Task<IActionResult> Index()
         {
+            ViewBag.Partners = partnerRepo.Partners;
+            ViewBag.SocialMedias = footerSocialMediaRepo.FooterSocialMedias;
+            ViewBag.Contacts = footerContactInfoRepo.FooterContactInfo;
+            ViewBag.AboutData = AboutUs;
+
+            return View(await _context.Gallery.ToListAsync());
+        }
+
+        // GET: Galleries/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            ViewBag.Partners = partnerRepo.Partners;
+            ViewBag.SocialMedias = footerSocialMediaRepo.FooterSocialMedias;
+            ViewBag.Contacts = footerContactInfoRepo.FooterContactInfo;
+            ViewBag.AboutData = AboutUs;
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var gallery = await _context.Gallery
+                .FirstOrDefaultAsync(m => m.GalleryID == id);
+            if (gallery == null)
+            {
+                return NotFound();
+            }
+
+            return View(gallery);
+        }
+
+        // GET: Galleries/Create
+        public IActionResult Create()
+        {
+            ViewBag.Partners = partnerRepo.Partners;
+            ViewBag.SocialMedias = footerSocialMediaRepo.FooterSocialMedias;
+            ViewBag.Contacts = footerContactInfoRepo.FooterContactInfo;
+            ViewBag.AboutData = AboutUs;
+
             return View();
         }
 
-        public IActionResult Update()
+        // POST: Galleries/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("GalleryID,BeforeImage,AfterImage")] Gallery gallery)
         {
-            return View();
+            ViewBag.Partners = partnerRepo.Partners;
+            ViewBag.SocialMedias = footerSocialMediaRepo.FooterSocialMedias;
+            ViewBag.Contacts = footerContactInfoRepo.FooterContactInfo;
+            ViewBag.AboutData = AboutUs;
+
+            if (ModelState.IsValid)
+            {
+                _context.Add(gallery);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(gallery);
+        }
+
+        // GET: Galleries/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            ViewBag.Partners = partnerRepo.Partners;
+            ViewBag.SocialMedias = footerSocialMediaRepo.FooterSocialMedias;
+            ViewBag.Contacts = footerContactInfoRepo.FooterContactInfo;
+            ViewBag.AboutData = AboutUs;
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var gallery = await _context.Gallery.FindAsync(id);
+            if (gallery == null)
+            {
+                return NotFound();
+            }
+            return View(gallery);
+        }
+
+        // POST: Galleries/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("GalleryID,BeforeImage,AfterImage")] Gallery gallery)
+        {
+            ViewBag.Partners = partnerRepo.Partners;
+            ViewBag.SocialMedias = footerSocialMediaRepo.FooterSocialMedias;
+            ViewBag.Contacts = footerContactInfoRepo.FooterContactInfo;
+            ViewBag.AboutData = AboutUs;
+
+            if (id != gallery.GalleryID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(gallery);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!GalleryExists(gallery.GalleryID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(gallery);
+        }
+
+        // GET: Galleries/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            ViewBag.Partners = partnerRepo.Partners;
+            ViewBag.SocialMedias = footerSocialMediaRepo.FooterSocialMedias;
+            ViewBag.Contacts = footerContactInfoRepo.FooterContactInfo;
+            ViewBag.AboutData = AboutUs;
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var gallery = await _context.Gallery
+                .FirstOrDefaultAsync(m => m.GalleryID == id);
+            if (gallery == null)
+            {
+                return NotFound();
+            }
+
+            return View(gallery);
+        }
+
+        // POST: Galleries/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            ViewBag.Partners = partnerRepo.Partners;
+            ViewBag.SocialMedias = footerSocialMediaRepo.FooterSocialMedias;
+            ViewBag.Contacts = footerContactInfoRepo.FooterContactInfo;
+            ViewBag.AboutData = AboutUs;
+
+            var gallery = await _context.Gallery.FindAsync(id);
+            _context.Gallery.Remove(gallery);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool GalleryExists(int id)
+        {
+            ViewBag.Partners = partnerRepo.Partners;
+            ViewBag.SocialMedias = footerSocialMediaRepo.FooterSocialMedias;
+            ViewBag.Contacts = footerContactInfoRepo.FooterContactInfo;
+            ViewBag.AboutData = AboutUs;
+
+            return _context.Gallery.Any(e => e.GalleryID == id);
         }
     }
 }
---
+++ MintGarage/Migrations/20211014015900_GallaryTable.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MintGarage.Migrations
+{
+    public partial class GallaryTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
