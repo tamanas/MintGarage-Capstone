f9bf226aa94e29102a7f6785914cf28065d09c40
dawoode <dawoode@sheridancollege.ca>
2021-11-22 13:46:48 -0500

    Password Encryption + Decryption

--- MintGarage/Controllers/AccountController.cs
+++ MintGarage/Controllers/AccountController.cs
@@ -7,6 +7,10 @@ using System.Threading.Tasks;
 using MintGarage.Models.Partners;
 using MintGarage.Models.FooterContents.FooterSocialMedias;
 using MintGarage.Models.FooterContents.FooterContactInfo;
+using System.Security.Cryptography;
+using System.IO;
+using System.Text;
+using System.Diagnostics;
 
 namespace MintGarage.Controllers
 {
@@ -19,6 +23,8 @@ namespace MintGarage.Controllers
 
         private const String AboutUs = "We are specialists in transforming and organizing any room. " +
         "We take pride in delivering outstanding quality and unique designs for our clients Across Canada & North America.";
+
+        private String pa;
 
         public AccountController(IAccountRepository accountRepo, IPartnerRepository partnerRepo, 
             IFooterContactInfoRepository footerContactInfoRepo, IFooterSocialMediaRepository footerSocialMediaRepo)
@@ -40,6 +46,52 @@ namespace MintGarage.Controllers
             return View();
         }
 
+        // Decryption
+        public string Decrypt(string cipherText)
+        {
+            string EncryptionKey = "MAKV2SPBNI99212";
+            byte[] cipherBytes = Convert.FromBase64String(cipherText);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(cipherBytes, 0, cipherBytes.Length);
+                        cs.Close();
+                    }
+                    cipherText = Encoding.Unicode.GetString(ms.ToArray());
+                }
+            }
+            return cipherText;
+        }
+
+        // Encryption
+        public string Encrypt(string clearText)
+        {
+            string EncryptionKey = "MAKV2SPBNI99212";
+            byte[] clearBytes = Encoding.Unicode.GetBytes(clearText);
+            using (Aes encryptor = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(clearBytes, 0, clearBytes.Length);
+                        cs.Close();
+                    }
+                    clearText = Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return clearText;
+        }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Login(Account account)
@@ -47,10 +99,35 @@ namespace MintGarage.Controllers
             ViewBag.Partners = partnerRepository.Partners;
             ViewBag.SocialMedias = footerSocialMediaRepository.FooterSocialMedias;
             ViewBag.Contacts = footerContactInfoRepository.FooterContactInfo;
-            ViewBag.AboutData = AboutUs;
+            ViewBag.AboutData = AboutUs;         
+
             if (ModelState.IsValid)
-            {
+            {   
                 Account acc = accoutRepository.Account.FirstOrDefault();
+                Debug.WriteLine(account.Password + account.Username);
+                Debug.WriteLine("acc"+acc.Password + acc.Username);
+                // Decryption
+                string EncryptionKey = "MAKV2SPBNI99212";
+                byte[] cipherBytes = Convert.FromBase64String(acc.Password);
+                    using (Aes encryptor = Aes.Create())
+                    {
+                        Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                        encryptor.Key = pdb.GetBytes(32);
+                        encryptor.IV = pdb.GetBytes(16);
+                        using (MemoryStream ms = new MemoryStream())
+                        {
+                            using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateDecryptor(), CryptoStreamMode.Write))
+                            {
+                                cs.Write(cipherBytes, 0, cipherBytes.Length);
+                                cs.Close();
+                            }
+                            acc.Password = Encoding.Unicode.GetString(ms.ToArray());
+                        Debug.WriteLine(account.Password + account.Username);
+                        Debug.WriteLine("acc" + acc.Password + acc.Username);
+                    }
+
+                    }
+
                 if (acc.Username == account.Username && acc.Password == account.Password)
                 {
                     return RedirectToAction("Update", "Home");
@@ -64,6 +141,8 @@ namespace MintGarage.Controllers
             }
             return View(account);
         }
+
+
 
         public IActionResult Update()
         {
@@ -83,24 +162,30 @@ namespace MintGarage.Controllers
             ViewBag.SocialMedias = footerSocialMediaRepository.FooterSocialMedias;
             ViewBag.Contacts = footerContactInfoRepository.FooterContactInfo;
             ViewBag.AboutData = AboutUs;
+
+            Account acc = accoutRepository.Account.FirstOrDefault();
+            string oldpass = Decrypt(acc.Password);
+            string newpass = Encrypt(updatePassword.NewPassword);
+
             if (ModelState.IsValid)
             {
-                Account acc = accoutRepository.Account.FirstOrDefault();
                 
-                if (!acc.Password.Equals(updatePassword.CurrectPassword))
+                
+                if (!oldpass.Equals(updatePassword.CurrectPassword))
                 {
                     TempData["Message"] = "Incorrect current password. Please try again!";
                     TempData["Success"] = false;
                 }
-                if (acc.Password.Equals(updatePassword.CurrectPassword))
+                if (oldpass.Equals(updatePassword.CurrectPassword))
                 {
-                    acc.Password = updatePassword.NewPassword;
+                    acc.Password = newpass;
                     accoutRepository.Update(acc);
                     TempData["Message"] = "Password updated successfully.";
                     TempData["Success"] = true;
-                }
+                }                
                 return RedirectToAction("Update");
             }
+ 
             return View();
         }
 
